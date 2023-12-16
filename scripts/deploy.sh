5b802b8155762a23368f0d1d22269b8a8b726e6a
Miguel Martin <miguel.martin@siptize.com>
2023-12-16 17:21:22 +0100

    Deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,14 @@ cp ./src/index.html ./dist/index.html
 file_path="./dist/index.html"
 sed -i 's|./dist||g' "$file_path"
 
-cp ./i18n ./dist/i18n -r
+# Copy the i18n folder from root to the dist folder
+mkdir -p ./dist/i18n/ && cp -r ./i18n/* ./dist/i18n/
 
 file_path="./dist/js/app.js"
 sed -i 's|\.\./\.\./i18n/|\.\./i18n/|g' "$file_path"
+
+git commit -am "Deploy"
+git push origin main
 
 # Push to the deploy branch
 git subtree push --prefix dist origin deploy
